7a867bdcf2f547445216de42f942947848b99c02
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-10-07 12:51:13 +0300

    There was no controller in the project, it was written in the route page, but I separated it, opened the controller page and made the code readable.

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,3 +1,87 @@
 const express = require("express");
 const Post = require("../Models/Post");
 const User = require("../Models/User");
+
+//create a post
+exports.createPost = async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//update a post
+exports.updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//delete a post
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//like / dislike a post
+exports.LikeOrDislike = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      //likes dediğimiz kısım post modelindeki likes
+      await post.updateOne({ $push: { likes: req.body.userId } }); //daha önce o  post'a like atıp atmadığını kontrol edip ona göre push'luyor
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } }); //dislike atarsa like sayısınından azaltıyor
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//get a post
+exports.getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//get timeline posts
+exports.getTimelinePost = async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
